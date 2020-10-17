7047696020562567a6bf3328f72af2edcf10ea8a
alex <aleksandr.Moshinec1@yandex.ru>
2020-10-17 14:22:44 +0300

    fix

--- RzrSite.DAL/Repositories/DbFileRepo.cs
+++ RzrSite.DAL/Repositories/DbFileRepo.cs
@@ -9,92 +9,93 @@ using System.Linq;
 
 namespace RzrSite.DAL.Repositories
 {
-  public class DbFileRepo : IDbFileRepo
-  {
-    private readonly RzrSiteDbContext _ctx;
-    private readonly IMapper _mapper;
-
-    public DbFileRepo(RzrSiteDbContext ctx, IMapper mapper)
+    public class DbFileRepo : IDbFileRepo
     {
-      _ctx = ctx;
-      _mapper = mapper;
+        private readonly RzrSiteDbContext _ctx;
+        private readonly IMapper _mapper;
+
+        public DbFileRepo(RzrSiteDbContext ctx, IMapper mapper)
+        {
+            _ctx = ctx;
+            _mapper = mapper;
+        }
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public IEnumerable<IStrippedDbFile> GetAll()
+        {
+            var files = _ctx.Files.ToList();
+
+            return _mapper.Map<IEnumerable<IStrippedDbFile>>(files);
+        }
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public IStrippedDbFile Get(int id)
+        {
+            if (!_ctx.Files.Any(c => c.Id.Equals(id)))
+                throw new EntityNotFoundException($"File :{id}: not found");
+            var file = _ctx.Files.Find(id);
+
+            return _mapper.Map<IStrippedDbFile>(file);
+        }
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public IDbFile Get(string path)
+        {
+            if (path == null) return null;
+            if (!_ctx.Files.Any(p => p.Path.ToLower() == path.ToLower()))
+                throw new EntityNotFoundException($"File :{path}: not found");
+            var file = _ctx.Files.First(f => path.ToLower() == f.Path.ToLower());
+
+            return file;
+        }
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public int? Add(IPostDbFile file)
+        {
+            var model = _mapper.Map<DbFile>(file);
+            var result = _ctx.Files.Add(model);
+            _ctx.SaveChanges();
+            return result.Entity?.Id;
+        }
+
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public IStrippedDbFile Update(int id, IPutDbFile fileChanges)
+        {
+            if (!_ctx.Files.Any(c => c.Id.Equals(id)))
+                throw new EntityNotFoundException($"File :{id}: not found");
+            var file = _ctx.Files.Find(id);
+            file = _mapper.Map(fileChanges, file);
+
+            _ctx.SaveChanges();
+
+            return _mapper.Map<IStrippedDbFile>(file);
+        }
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public bool Delete(int id)
+        {
+            if (!_ctx.Files.Any(c => c.Id.Equals(id)))
+                throw new EntityNotFoundException($"File :{id}: not found");
+
+            var file = _ctx.Files.Find(id);
+
+            _ctx.Files.Remove(file);
+            _ctx.SaveChanges();
+
+            return true;
+        }
     }
-
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    public IEnumerable<IStrippedDbFile> GetAll()
-    {
-      var files = _ctx.Files.ToList();
-
-      return _mapper.Map<IEnumerable<IStrippedDbFile>>(files);
-    }
-
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    public IStrippedDbFile Get(int id)
-    {
-      if (!_ctx.Files.Any(c => c.Id.Equals(id)))
-        throw new EntityNotFoundException($"File :{id}: not found");
-      var file = _ctx.Files.Find(id);
-      
-      return _mapper.Map<IStrippedDbFile>(file);
-    }
-
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    public IDbFile Get(string path)
-    {
-      if (!_ctx.Files.Any(p => p.Path.ToLower() == path.ToLower()))
-        throw new EntityNotFoundException($"File :{path}: not found");
-      var file = _ctx.Files.First(f => path.ToLower() == f.Path.ToLower());
-      
-      return file;
-    }
-
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    public int? Add(IPostDbFile file)
-    {
-      var model = _mapper.Map<DbFile>(file);
-      var result = _ctx.Files.Add(model);
-      _ctx.SaveChanges();
-      return result.Entity?.Id;
-    }
-
-
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    public IStrippedDbFile Update(int id, IPutDbFile fileChanges)
-    {
-      if (!_ctx.Files.Any(c => c.Id.Equals(id)))
-        throw new EntityNotFoundException($"File :{id}: not found");
-      var file = _ctx.Files.Find(id);
-      file = _mapper.Map(fileChanges, file);
-
-      _ctx.SaveChanges();
-
-      return _mapper.Map<IStrippedDbFile>(file);
-    }
-
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    public bool Delete(int id)
-    {
-      if (!_ctx.Files.Any(c => c.Id.Equals(id)))
-        throw new EntityNotFoundException($"File :{id}: not found");
-
-      var file = _ctx.Files.Find(id);
-
-      _ctx.Files.Remove(file);
-      _ctx.SaveChanges();
-
-      return true;
-    }
-  }
 }
