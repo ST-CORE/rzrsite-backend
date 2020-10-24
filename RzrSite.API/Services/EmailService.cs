bb2d8c95108f810812c6986ba4d1f45a5b162d62
alex <aleksandr.Moshinec1@yandex.ru>
2020-10-24 23:07:38 +0300

    send email messages

---
+++ RzrSite.API/Services/EmailService.cs
@@ -0,0 +1,47 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+
+namespace RzrSite.API.Services
+{
+    public class EmailService : IEmailService
+    {
+        private readonly EmailServiceConfig _config;
+
+        public EmailService(IOptions<EmailServiceConfig> config)
+        {
+            _config = config.Value;
+        }
+
+        public async Task SendEmailAsync(string subject, string message, string email = null)
+        {
+            var emailMessage = CreateMessage(subject, message, email);
+            using var client = new SmtpClient();
+            await client.ConnectAsync(_config.Host, _config.Port, true);
+            await client.AuthenticateAsync(_config.From, _config.Password);
+            await client.SendAsync(emailMessage);
+            await client.DisconnectAsync(true);
+        }
+
+        private MimeMessage CreateMessage(string subject, string message, string email = null)
+        {
+            var emailMessage = new MimeMessage();
+            emailMessage.From.Add(new MailboxAddress(_config.Name, _config.From));
+            emailMessage.To.Add(new MailboxAddress(string.Empty, email ?? _config.Receiver));
+            emailMessage.Subject = subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = message };
+            return emailMessage;
+        }
+    }
+
+    public class EmailServiceConfig
+    {
+        public string Name { get; set; }
+        public string From { get; set; }
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public string Password { get; set; }
+        public string Receiver { get; set; }
+    }
+}
---
+++ RzrSite.API/Services/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace RzrSite.API.Services
+{
+    public interface IEmailService
+    {
+        Task SendEmailAsync(string subject, string message, string email = null);
+    }
+}
